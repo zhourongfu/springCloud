e43b52f23423cf9d1622630cac001e00744f44a1
ssy8110@126.com <3by96_Hha5>
2018-05-17 16:26:12 +0800

    sleuth日志跟踪

--- serviceCustomerA/src/main/java/serviceCustomer/CustomerFeignController.java
+++ serviceCustomerA/src/main/java/serviceCustomer/CustomerFeignController.java
@@ -1,5 +1,7 @@
 package serviceCustomer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,13 +12,14 @@ import java.util.Map;
 
 @RestController
 public class CustomerFeignController {
-	
+	private static Logger logger = LoggerFactory.getLogger(CustomerFeignController.class);
 	@Autowired
 	FeignClientService service;
 	
 	@RequestMapping("test")
 	public String test(String s){
 		Map<String,String> params = Collections.singletonMap("name", "jhon");
+		logger.info("     ====>"+params);
 		if("1".equals(s))
 		return service.sayHello(params);
 		else return service.hiMan(params);
