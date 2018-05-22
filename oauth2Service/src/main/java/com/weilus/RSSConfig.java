1484169fd32684de6a42baa822c5f84003a93e6b
ssy8110@126.com <3by96_Hha5>
2018-05-22 17:57:27 +0800

    oauth2.0

--- oauth2Service/src/main/java/com/weilus/RSSConfig.java
+++ oauth2Service/src/main/java/com/weilus/RSSConfig.java
@@ -2,10 +2,13 @@ package com.weilus;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
 /**
  * Created by liutq on 2018/5/22.
@@ -15,12 +18,20 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 public class RSSConfig extends ResourceServerConfigurerAdapter {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER");
+        auth.inMemoryAuthentication()
+            .withUser("user").password("password").roles("USER");
     }
+    @Autowired
+    private RedisConnectionFactory connectionFactory;
+
+    @Autowired
+    public void configureGlobal(AuthorizationServerEndpointsConfigurer endpoints){
+        endpoints.tokenStore(new RedisTokenStore(connectionFactory));
+    }
+
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.antMatcher("/user").authorizeRequests().anyRequest().authenticated();
     }
+
 }
