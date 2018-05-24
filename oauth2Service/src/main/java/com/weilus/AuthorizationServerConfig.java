626993b6bc18709295fd319edbec306ba99da0b3
ssy8110@126.com <3by96_Hha5>
2018-05-24 18:01:33 +0800

    zuul+oauth2.0

--- oauth2Service/src/main/java/com/weilus/AuthorizationServerConfig.java
+++ oauth2Service/src/main/java/com/weilus/AuthorizationServerConfig.java
@@ -2,6 +2,8 @@ package com.weilus;
 
 import com.weilus.service.Auth2ClientService;
 import com.weilus.service.Auth2UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -11,6 +13,14 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by liutq on 2018/5/23.
@@ -18,7 +28,7 @@ import org.springframework.security.oauth2.provider.token.store.redis.RedisToken
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
-
+    private static final Logger logger = LoggerFactory.getLogger(AuthorizationServerConfig.class);
     @Autowired
     private AuthenticationManager authenticationManager;
     @Autowired
@@ -39,6 +49,35 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
         tokenServices.setClientDetailsService(auth2ClientService);
         tokenServices.setAuthenticationManager(authenticationManager);
         endpoints.tokenServices(tokenServices);
+        endpoints.addInterceptor(new HandlerInterceptor() {
+            @Override
+            public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+                Enumeration<String> e = request.getHeaderNames();
+                Map<String,String> headers = new HashMap<>();
+                while (e.hasMoreElements()){
+                    String headerName = e.nextElement();
+                    headers.put(headerName,request.getHeader(headerName));
+                }
+                logger.info(headers.toString());
+                return true;
+            }
+
+            @Override
+            public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+            }
+
+            @Override
+            public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+                Enumeration<String> e = request.getHeaderNames();
+                Map<String,String> headers = new HashMap<>();
+                while (e.hasMoreElements()){
+                    String headerName = e.nextElement();
+                    headers.put(headerName,request.getHeader(headerName));
+                }
+                logger.info(headers.toString());
+            }
+        });
     }
 
     @Override
