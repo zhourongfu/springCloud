dd2af611b89d6fd73c7d129d8e0901df3908e43e
weilus923 <646154945@qq.com>
2018-05-26 15:16:55 +0800

    oauth2.0

--- apiGateWay/src/main/java/com/weilus/SecurityConfig.java
+++ apiGateWay/src/main/java/com/weilus/SecurityConfig.java
@@ -1,19 +1,66 @@
 package com.weilus;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
+import org.springframework.web.client.DefaultResponseErrorHandler;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
 
 /**
  * Created by liutq on 2018/5/25.
  */
+@EnableEurekaClient
+@RibbonClient(name = "uaa")
 @Configuration
 @EnableResourceServer
 public class SecurityConfig extends ResourceServerConfigurerAdapter {
+
+
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers(HttpMethod.GET, "/hello").authenticated();
     }
+
+
+
+    @LoadBalanced
+    @Bean("remoteTokenRestTemplate")
+    public RestTemplate restTemplate(){
+        RestTemplate restTemplate = new RestTemplate();
+        ((RestTemplate) restTemplate).setErrorHandler(new DefaultResponseErrorHandler() {
+            @Override
+            // Ignore 400
+            public void handleError(ClientHttpResponse response) throws IOException {
+                if (response.getRawStatusCode() != 400) {
+                    super.handleError(response);
+                }
+            }
+        });
+        return restTemplate;
+    }
+
+    @Primary
+    @Bean
+    @Autowired
+    @Qualifier("remoteTokenRestTemplate")
+    public RemoteTokenServices remoteTokenServices(RestTemplate restTemplate){
+        RemoteTokenServices services = new RemoteTokenServices();
+        services.setCheckTokenEndpointUrl("http://uaa/oauth/check_token");
+        services.setRestTemplate(restTemplate);
+        return services;
+    }
+
 }
