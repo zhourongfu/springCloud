9961fe86d87d24e952e4bae7a79ab0883efcb1ec
刘太全 <646154945@qq.com>
2018-12-11 17:28:52 +0800

    feign

--- feign-hystrix/src/main/java/com/weilus/Application.java
+++ feign-hystrix/src/main/java/com/weilus/Application.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,6 +14,8 @@ import java.util.Collections;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients(basePackages="com.feign")
+@EnableHystrix
 @Controller
 public class Application {
 
