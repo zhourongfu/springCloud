5f2cd552094216042efd72f793067c74faf5718d
刘太全 <646154945@qq.com>
2018-12-11 15:59:52 +0800

    oauth

--- oauth/data-mysql.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO T_AUTH2_CLIENT(ID,CLIENT_ID,CLIENT_SECRET,SCOPES,GRANT_TYPES,ACCESS_TOKEN_VALIDITY_SECONDS,REFRESH_TOKEN_VALIDITY_SECONDS)
-VALUES
-(1, 'acme',   'acmesecret',  'read,write', 'password,refresh_token',          180, 3600),
-(2, 'sdfaesa','aasdf5#%asdf','read,write', 'client_credentials,refresh_token',200, 4000);
-
-INSERT INTO T_USER(ID,USERNAME,USERPWD,LOCKED,ENABLE,ACCOUNTEXPIRED)
- VALUES (1, 'admin', 'weilus', 0, 0, 0);
