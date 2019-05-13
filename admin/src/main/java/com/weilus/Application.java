67f5a70ed9e0969c460c7eb25293ee3659872f04
刘太全 <646154945@qq.com>
2019-05-13 17:27:51 +0800

    admin

--- admin/src/main/java/com/weilus/Application.java
+++ admin/src/main/java/com/weilus/Application.java
@@ -4,6 +4,9 @@ import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * @author 1
@@ -17,5 +20,29 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+
+    @Configuration
+    public static class SecurityConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            // Page with login form is served as /login.html and does a POST on /login
+            http.formLogin().loginPage("/login.html").loginProcessingUrl("/login").permitAll();
+            // The UI does a POST on /logout on logout
+            http.logout().logoutUrl("/logout");
+            // The ui currently doesn't support csrf
+            http.csrf().disable();
+
+            // Requests for the login page and the static assets are allowed
+            http.authorizeRequests()
+                    .antMatchers("/login.html", "/**/*.css", "/img/**", "/third-party/**","/api/**")
+                    .permitAll();
+            // ... and any other request needs to be authorized
+            http.authorizeRequests().antMatchers("/**").authenticated();
+
+            // Enable so that the clients can authenticate via HTTP basic for registering
+            http.httpBasic();
+        }
     }
 }
