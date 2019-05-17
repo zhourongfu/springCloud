a9b25c5060b238884d467c602a08ac5cda6770b0
刘太全 <646154945@qq.com>
2019-05-17 12:48:46 +0800

    auth

--- oauth-resource/src/main/java/com/feign/clients/OauthClient.java
+++ oauth-resource/src/main/java/com/feign/clients/OauthClient.java
@@ -1,7 +1,7 @@
 package com.feign.clients;
 
 import com.feign.fallbacks.OauthClientFallBack;
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
--- oauth-resource/src/main/java/com/weilus/Application.java
+++ oauth-resource/src/main/java/com/weilus/Application.java
@@ -3,7 +3,7 @@ package com.weilus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
