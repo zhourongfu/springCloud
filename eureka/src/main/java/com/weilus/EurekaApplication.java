2d6e174b650eeace4ead1bc7f811ff01857f773d
刘太全 <646154945@qq.com>
2019-05-17 11:15:11 +0800

    auth

--- eureka/src/main/java/com/weilus/EurekaApplication.java
+++ eureka/src/main/java/com/weilus/EurekaApplication.java
@@ -3,11 +3,24 @@ package com.weilus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableEurekaServer  //启动一个服务注册中心提供给其他应用进行对话
 @SpringBootApplication
 public class EurekaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaApplication.class, args);
+	}
+
+	@EnableWebSecurity
+	static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().ignoringAntMatchers("/eureka/**");
+			super.configure(http);
+		}
 	}
 }
