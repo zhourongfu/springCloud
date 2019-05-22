357ac0e0678cb46b84a1b0c23930f3a17277b945
刘太全 <646154945@qq.com>
2019-05-22 17:45:56 +0800

    bus

--- oauth-resource/src/main/java/com/weilus/Application.java
+++ oauth-resource/src/main/java/com/weilus/Application.java
@@ -1,14 +1,13 @@
 package com.weilus;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
@@ -22,16 +21,23 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Autowired
+	TestProperties properties;
 
-	@RequestMapping("/me")
-	@PreAuthorize("#oauth2.hasScope('oauth-test')")
-	public @ResponseBody Object getUser(){
-		return SecurityContextHolder.getContext().getAuthentication();
-	}
+//	@RequestMapping("/me")
+//	@PreAuthorize("#oauth2.hasScope('oauth-test')")
+//	public @ResponseBody Object getUser(){
+//		return SecurityContextHolder.getContext().getAuthentication();
+//	}
+//
+//	@RequestMapping("/client")
+//	@PreAuthorize("#oauth2.hasScope('resource')")
+//	public @ResponseBody Object client(){
+//		return SecurityContextHolder.getContext().getAuthentication();
+//	}
 
-	@RequestMapping("/client")
-	@PreAuthorize("#oauth2.hasScope('resource')")
-	public @ResponseBody Object client(){
-		return SecurityContextHolder.getContext().getAuthentication();
+	@GetMapping("test")
+	public @ResponseBody Object test(){
+		return properties;
 	}
 }
