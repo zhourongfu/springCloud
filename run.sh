3a3077b678c199bce1741f74cf663202c09dffdc
刘太全 <646154945@qq.com>
2019-05-27 11:45:37 +0800

    dockerfile

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
 !# /bin/bash
-docker stack deploy -c docker-compose.yml up
+# 单机启动 MYSQL REDIS MQ EUREKA ZUUL
+docker-compose -f docker-compose.yml up -d
+# swarm集群启动配置中心 认证中心
+docker stack deploy -c docker-stack.yml cloud --with-registry-auth
