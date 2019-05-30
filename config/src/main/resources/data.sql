b7521663a67889cc5401aafa59f76b5df3586787
刘太全 <646154945@qq.com>
2019-05-30 13:04:32 +0800

    gateway

--- config/src/main/resources/data.sql
+++ config/src/main/resources/data.sql
@@ -16,6 +16,16 @@ INSERT INTO cloud_properties(`key`,`value`,`application`,profile,label) VALUES
 ('zuul.routes.serviceProducer.serviceId','serviceProducer','zuul','test','master'),
 ('zuul.routes.serviceProducer.path','/serviceProducer/**','zuul','test','master');
 
+-- gateway-test-master.properties
+INSERT INTO cloud_properties(`key`,`value`,`application`,profile,label) VALUES
+('spring.rabbitmq.addresses','192.168.198.128:5672','gateway','test','master'),
+('spring.rabbitmq.username','guest','gateway','test','master'),
+('spring.rabbitmq.password','guest','gateway','test','master'),
+('spring.cloud.gateway.routes[0].id','oauth','gateway','test','master'),
+('spring.cloud.gateway.routes[0].uri','lb://oauth','gateway','test','master'),
+('spring.cloud.gateway.routes[0].predicates[0]','Path=/oauth/**','gateway','test','master'),
+('spring.cloud.gateway.routes[0].filters[0]','StripPrefix=1','gateway','test','master');
+
 -- serviceProducer-test-master.properties
 INSERT INTO cloud_properties(`key`,`value`,`application`,profile,label) VALUES
 ('spring.rabbitmq.addresses','192.168.198.128:5672','serviceProducer','test','master'),
